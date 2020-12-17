374abbdb5b3d2f8d00690dc54878b55977ab5db5
RuRuRu <rururutan+github@gmail.com>
2020-12-17 23:16:25 +0900

    ADPCMスタートアドレス指定時に即再生アドレスも更新するコードを外す https://twitter.com/kajaponn/status/1292470271295668224

--- src/devices/opna.cpp
+++ src/devices/opna.cpp
@@ -638,7 +638,7 @@ void OPNABase::SetADPCMBReg(uint addr, uint data)
 	case 0x03:		// Start Address H
 		adpcmreg[addr - 0x02 + 0] = data;
 		startaddr = (adpcmreg[1]*256+adpcmreg[0]) << 6;
-		memaddr = startaddr;
+//		memaddr = startaddr;
 //		LOG1("  startaddr %.6x", startaddr);
 		break;
 
