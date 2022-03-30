98b64c02f644f147b069a6128216ac5d3ed55335
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-31 00:59:30 +0800

    Delete sample2 directory

--- sample2/src/guid.cpp
+++
@@ -1,7 +0,0 @@
-// $Id: guid.cpp,v 1.2 1999/11/26 10:13:09 cisc Exp $
-
-#define INITGUID
-#include <objbase.h>
-#include "if/ifguid.h"
-
-
--- sample2/src/pch.cpp
+++
@@ -1,3 +0,0 @@
-// $Id: pch.cpp,v 1.1 1999/10/10 01:43:28 cisc Exp $
-#include "headers.h"
-#pragma hdrstop
