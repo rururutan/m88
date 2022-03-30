32460309a2f0dd1164eb133a26fece56de1cce79
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-31 00:58:18 +0800

    Delete cdif directory

--- cdif/src/guid.cpp
+++
@@ -1,7 +0,0 @@
-// $Id: guid.cpp,v 1.2 1999/11/26 10:12:48 cisc Exp $
-
-#define INITGUID
-#include <objbase.h>
-#include "if/ifguid.h"
-
-
--- cdif/src/pch.cpp
+++
@@ -1,3 +0,0 @@
-// $Id: pch.cpp,v 1.1 1999/08/26 08:04:38 cisc Exp $
-#include "headers.h"
-#pragma hdrstop
